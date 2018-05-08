483ce3ece822958f80424d398b9171e610714ea2
kevinta893 <kevinta893@gmail.com>
2018-05-08 01:23:09 -0600

    Add cleanup

--- backup_mcmyadmin.sh
+++ backup_mcmyadmin.sh
@@ -9,8 +9,11 @@ S3_BACKUP_URL="s3://some-s3-bucket"
 cd ~
 ZIP_BACKUP_DATE=`date '+%Y-%m-%d__%H-%M-%S'`
 ZIP_BACKUP_NAME="\"McMyAdmin-Backup-$ZIP_BACKUP_DATE.zip\""
-zip -r $ZIP_BACKUP_NAME ~/McMyAdmin 
+zip -r ~/$ZIP_BACKUP_NAME ~/McMyAdmin 
 
 
 #AWS S3 Backup
 /home/ubuntu/.local/bin/aws s3 cp ~/$ZIP_BACKUP_NAME $S3_BACKUP_URL
+
+#cleanup
+rm ~/$ZIP_BACKUP_NAME
--- backup_saves.sh
+++ backup_saves.sh
@@ -4,4 +4,5 @@ S3_BACKUP_URL="s3://some-s3-bucket"
 
 
 #AWS S3 Sync folder
+cd ~
 /home/ubuntu/.local/bin/aws s3 sync ~/McMyAdmin/Backups $S3_BACKUP_URL

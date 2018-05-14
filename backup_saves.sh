4942ebaf06d7c1ce298081e0eeb385efa4d7b41e
kevinta893 <kevinta893@gmail.com>
2018-05-14 00:42:03 -0600

    Added CD change failure case

--- backup_saves.sh
+++ backup_saves.sh
@@ -4,5 +4,5 @@ S3_BACKUP_URL="s3://some-s3-bucket"
 STORAGE_CLASS="STANDARD"			#choose one of:  STANDARD | REDUCED_REDUNDANCY | STANDARD_IA | ONEZONE_IA
 
 #AWS S3 Sync folder
-cd ~
+cd ~ || (echo "Script failed"; exit;)
 /home/ubuntu/.local/bin/aws s3 sync --storage-class $STORAGE_CLASS ~/McMyAdmin/Backups $S3_BACKUP_URL
